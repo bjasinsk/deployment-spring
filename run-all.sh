741c5d8411b08ad20cee9aca0c7d4af4f82b74bb
bjasinsk <01169292@pw.edu.pl>
2024-04-14 01:04:33 +0200

    change paths

--- run-all.sh
+++ run-all.sh
@@ -5,12 +5,12 @@ source config.sh
 front_username=$AZURE_VM_USERNAME
 front_password=$AZURE_VM_PASSWORD
 
-# backend_username=$AZURE_VM_USERNAME
-# backend_password=$AZURE_VM_PASSWORD
+backend_username=$AZURE_VM_USERNAME
+backend_password=$AZURE_VM_PASSWORD
 
 # database_username=$AZURE_VM_USERNAME
 # database_password=$AZURE_VM_PASSWORD
 
 ./run-front.sh "$front_password" "$front_username" "$angular_IP" "$angular_PORT" "$spring_IP" "$spring_PORT"
-# ./run-backend.sh "$backend_password" "$backend_username" "$spring_IP" "$spring_PORT"
-# ./run-db.sh "$database_password" "$database_username" "$db_IP" "$db_PORT"
+# ./run-back.sh "$backend_password" "$backend_username" "$spring_IP" "$spring_PORT" "$db_IP" "$db_PORT"
+# ./db.sh "$database_password" "$database_username" "$db_IP" "$db_PORT"
