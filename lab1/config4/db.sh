82c54156bc2c954e22234bf1e5c07f2da059b8bb
bjasinsk <01169292@pw.edu.pl>
2024-04-19 22:40:44 +0200

    add part of databases setup script

--- lab1/config4/db.sh
+++ lab1/config4/db.sh
@@ -1,33 +1,50 @@
 #!/bin/bash
 
+cd ~
+
 sudo apt-get update
-sudo apt-get install mysql-server -y
-sudo apt-get install wget -y
+sudo apt-get install -y mysql-server
+sudo apt-get install -y wget
 
 INIT_DB_PATH="https://raw.githubusercontent.com/spring-petclinic/spring-petclinic-rest/master/src/main/resources/db/mysql/initDB.sql"
 POPULATE_DB_PATH="https://raw.githubusercontent.com/spring-petclinic/spring-petclinic-rest/master/src/main/resources/db/mysql/populateDB.sql"
 wget $INIT_DB_PATH
 wget $POPULATE_DB_PATH
 
-sudo mysql -v -e "CREATE DATABASE petclinic_master;"
+configure_mysql() {
+    PORT=$1
+    DATABASE=$2
+    DATADIR=$3
 
-sudo mysql -v -e "CREATE USER 'pc_master'@'%' IDENTIFIED BY 'pc_master';"
-sudo mysql -v -e "GRANT ALL PRIVILEGES ON petclinic_master.* TO 'pc_master'@'%';"
-sudo mysql -v -e "FLUSH PRIVILEGES;"
+    sudo cp /etc/mysql/my.cnf /etc/mysql/my$PORT.cnf
+    sudo bash -c "echo '[mysqld]' >> /etc/mysql/my$PORT.cnf"
+    sudo bash -c "echo 'port = $PORT' >> /etc/mysql/my$PORT.cnf"
+    sudo bash -c "echo 'socket = /var/run/mysqld/mysqld$PORT.sock' >> /etc/mysql/my$PORT.cnf"
+    sudo bash -c "echo 'datadir = $DATADIR' >> /etc/mysql/my$PORT.cnf"
+    sudo mkdir -p $DATADIR
 
-sed -i '7d' ./initDB.sql
-cat ./populateDB.sql >> ./initDB.sql
-sudo mysql < ./initDB.sql
-sudo service mysql restart
-sudo sed -i 's/port\t\t\t= 3306/port\t\t\t= 3307/g' /etc/mysql/mysql.conf.d/mysqld.cnf
-sudo service mysql restart
+    # JEST OK -----------------------
 
-sudo mysql -v -e "CREATE DATABASE petclinic_slave;"
-sudo mysql -v -e "CREATE USER 'pc_slave'@'%' IDENTIFIED BY 'pc_slave';"
-sudo mysql -v -e "GRANT ALL PRIVILEGES ON petclinic_slave.* TO 'pc_slave'@'%';"
-sudo mysql -v -e "FLUSH PRIVILEGES;"
+    # sudo mysqld --initialize --user=mysql --basedir=/usr --datadir=$DATADIR
+    
+    # poszukać komend mysqlowych do stawiania servera
+    sudo systemctl start mysql@$PORT
 
-echo "Finished database setup"
+    sudo mysql --port=$PORT -e "CREATE DATABASE $DATABASE;"
+    sudo mysql --port=$PORT -e "CREATE USER 'pc'@'%' IDENTIFIED BY 'petclinic';"
+    sudo mysql --port=$PORT -e "GRANT ALL PRIVILEGES ON $DATABASE.* TO 'pc'@'%';"
+    sudo mysql --port=$PORT -e "FLUSH PRIVILEGES;"
+    
+    sed -i '7d' initDB.sql
+    cat populateDB.sql >> initDB.sql
+    sudo mysql --port=$PORT $DATABASE < initDB.sql
 
+    sudo systemctl restart mysql@$PORT
 
+    sudo mysql --port=$PORT -e "UNLOCK TABLES;"
 
+    echo "Finished setting up the database $DATABASE on port $PORT"
+}
+
+configure_mysql 3306 petclinic_post /var/lib/mysql_3306
+configure_mysql 3307 petclinic_get /var/lib/mysql_3307
